2934dd5b252e8d6116903569375948d5ddf9e8bb
saumil <saumiljoshi64@gmail.com>
2021-06-03 17:56:35 +0530

    skipped staging area and fixed

--- create.php
+++ create.php
@@ -6,33 +6,7 @@ include("dbconnection.php");
 //if($conn){
    // echo "Connection established";
 //}
-if(isset($_POST['submit'])) {
-    $filename=$_FILES["uploadfile"]["name"];
-    $tmpfile=$_FILES["uploadfile"]["tmp_name"];
-    $folder = "product_image/".$filename;
-    move_uploaded_file($tmpfile,$folder);
-    $Name = $_POST['name'];
-    $category = $_POST['category'];
-    $Brand = $_POST["Brand"];
-    $price = $_POST["price"];
-    $display_price = $_POST['Display-price'];
-    $Description = $_POST['description'];
-   
-   if($filename!="" && $Name!="" && $category!="" && $Brand!="" && $price!="" && $display_price!="" && $Description!="") {
-       $query = "INSERT INTO `product_details` (`id`, `image`, `name`, `category`, `brand`, `price`, `display_price`, `description`) VALUES (NULL, '$folder', '$Name', '$category', '$Brand', '$price', '$display_price', '$Description');";
-      // echo $query;
-      // exit();
-       $data = mysqli_query($conn,$query);
-    }
-    else 
-    {
-      echo "All fields are required";
-    }
-    if($data){
-        echo "data inserted into database";
-    }
-        
-}
+
 
 ?>
 <!DOCTYPE html>
@@ -42,29 +16,58 @@ if(isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>product_crud</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body>
-<div class="nav-bar" style="margin-top:200px; margin-left:700px; margin-right:100px; margin-bottom:100px;" class = "form-group">
+<div class="card">
+<div class="card-header bg-dark"><h1 class="text-white text-center">Insert operation</h1>
+</div></div>
+<div class="container" style="margin-top:50px; margin-left:700px; margin-right:500px; margin-bottom:100px;">
 <form action="" method="post" enctype="multipart/form-data">
-<div class="container" class= "nav-nav-tabs">
 <label for="image-file">image-file</label>
-<input type="file" name="uploadfile" class = "form-control" required/><?php echo "<img src='$folder' height='100' width='100'/>";?><br/><br/>
-</div>
-<div class="container" class="nav-nav-tabs">
+<input type="file" name="uploadfile" required/><?php echo "<img src='$folder' height='100' width='100'/>";?><br/><br/>
 <label for="name">full name</label>
-<input type="text" name="name" class = "form-control" required/><br/><br/></div>
-<div class="container" class= "nav-nav-tabs">
+<input type="text" name="name" style="width: 500px; height: 60px;" class="form-control" required/><br/><br/>
 <label for="name">Category</label>
-<input type="text" name="category" class = "form-control" required/><br/><br/></div>
+<input type="text" name="category" style="width: 500px; height: 50px;" class="form-control"  required/><br/><br/>
 <label for="name">Brand</label>
-<input type="text" name="Brand" class = "form-control" required/><br/><br/>
+<input type="text" name="Brand" style="width: 500px; height: 50px;" class="form-control"  required/><br/><br/>
 <label for="name">Price</label>
-<input type="text" name="price" class="form-control" required/><br/><br/>
+<input type="text" name="price" style="width: 500px; height: 50px;" class="form-control"  required/><br/><br/>
 <label for="name">Display-price</label>
-<input type="text" name="Display-price" class="form-control" required/><br/><br/>
+<input type="text" name="Display-price" style="width: 500px; height: 50px;" class="form-control"  required/><br/><br/>
 <label for="description">Description</label>
-<textarea name="description" class="form-control" required></textarea><br/><br/>
-<input type="submit" name="submit" class="btn btn-primary" required/><br/>
+<textarea name="description" style="width: 500px; height: 50px;" class="form-control"  required></textarea><br/><br/>
+<input type="submit" name="submit" class="btn btn-primary" required/><br/><br/>
 </form>
+<?php 
+if(isset($_POST['submit'])) {
+  $filename=$_FILES["uploadfile"]["name"];
+  $tmpfile=$_FILES["uploadfile"]["tmp_name"];
+  $folder = "product_image/".$filename;
+  move_uploaded_file($tmpfile,$folder);
+  $Name = $_POST['name'];
+  $category = $_POST['category'];
+  $Brand = $_POST["Brand"];
+  $price = $_POST["price"];
+  $display_price = $_POST['Display-price'];
+  $Description = $_POST['description'];
+  $query = "INSERT INTO `product_details` (`id`, `image`, `name`, `category`, `brand`, `price`, `display_price`, `description`) VALUES (NULL, '$folder', '$Name', '$category', '$Brand', '$price', '$display_price', '$Description');";
+    // echo $query;
+    // exit();
+  $data = mysqli_query($conn,$query);
+  if($data){
+      echo "data inserted into database";
+  }
+  
+      
+}else 
+{
+echo "<font color='blue'>All fields are required</font>";
+}
+
+?>
 </body>
 </html>
---
+++ update.php
@@ -0,0 +1,66 @@
+<?php 
+include("dbconnection.php"); 
+//echo $_GET['id']; echo"<br/>";
+ //echo $_GET ['Description'];echo"<br/>"; 
+ if(isset($_POST['edit'])){
+
+   $id = $_GET['id'];  
+   $Name = $_POST['name'];
+    $category = $_POST['category'];
+    $Brand = $_POST['brand'];
+    $Img = $_FILES["Uploadfile"]["name"];
+    $tmpfile=$_FILES["Uploadfile"]["tmp_name"];
+    $folder = "product_image/".$Img;
+    move_uploaded_file($tmpfile,$folder);
+    $Price = $_POST['Price'];
+    $display_price = $_POST['display_price'];
+    $Desc = $_POST['description'];
+    $update =  "UPDATE product_details SET name='$Name',category='$category',brand='$Brand', image='$folder',price='$Price',display_price='$display_price',description='$Desc' WHERE id='$id'" ;
+     // $update="UPDATE 'product_details' SET 'name' = '$Name','category' = '$category','brand' = '$Brand','price' = '$Price','display_price' = '$display_price','description' = '$Desc' WHERE  'id' = $id";
+    //echo( $update);
+    //exit();
+    //UPDATE `product_details` SET `name` = 'aamir', `category` = 'frg44', `brand` = '55666', `price` = '333', `display_price` = '666' WHERE `product_details`.`id` = 34;
+    $query = mysqli_query($conn,$update);
+    if($query){
+        echo "record has been updated successfully";
+      }
+      else
+      {
+        echo "not updated successfully";
+      } 
+}
+    else {
+     echo "<center><font color='blue'>click on button</font></center>";
+   }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>product_crud</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="container" class= "col-lg-12 m-auto" class="form-group">
+    <form method="POST" action="" enctype="multipart/form-data">
+    <label for="name">name:</label>
+    <input type="text" name="name" placeholder="insert your name here"   class="form-control"/><br/>
+    <label for="category">category:</label>
+    <input type="text" name="category" placeholder="insert your data here" class="form-control"/><br/>
+    <input type ="file" name="Uploadfile"/><br/>
+    <label for="category">Brand:</label>
+    <input type="text" name="brand" placeholder="insert your data here"  class="form-control"/><br/>
+    <label for="category">Price:</label>
+    <input type="text" name="Price" placeholder="insert your"  class="form-control"/><br/>
+    <label for="category">display_price:</label>
+    <input type="text" name="display_price" placeholder="insert" class="form-control"/><br/>
+    <label for="category">Description:</label>
+    <input type="textarea" name="description" placeholder="insert here"  class="form-control" style="width:200px; margin-right:100px; margin-bottom:10px; height:100px;" /><br/>
+    <input type="submit" name="edit" class="btn btn-primary" value="update"/><br/>
+    </form>
+</div>
+</body>
+</html>
